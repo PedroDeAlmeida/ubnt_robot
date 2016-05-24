c43dbd3b674cc871855c4968ea8afa4207560b22
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-23 22:43:38 -0300

    Primeira versao de script1.sh

--- script1.sh
+++ script1.sh
@@ -1,1 +1,16 @@
+echo "*** Iniciando ***"
+rm /tmp/tempconfig
+
+echo "*** Apagando linha de configuracoes do discovery ***"
+grep -v "discovery" /tmp/system.cfg > /tmp/tempconfig
+
+echo "*** Inserindo nova configuracao ***"
+echo "discovery.cdp.status=disabled" >> /tmp/tempconfig
+echo "discovery.status=disabled" >> /tmp/tempconfig
+
+echo "*** Sanvando na Flash ***"
 cfgmtd -w -p /etc/
+save
+
+echo "*** Reiniciando ***"
+reboot
