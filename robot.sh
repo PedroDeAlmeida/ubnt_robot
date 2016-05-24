56d4c5c6a97d3866c67f83792b9b19676f9c4d75
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-24 05:42:52 -0300

    Ajuste final no arquivo robot.sh

--- robot.sh
+++ robot.sh
@@ -3,14 +3,16 @@
 
 port=22
 
-user=
-pass=
-network=""
-ip=1
+user=cliente
+pass=radio@cliente
+network="172.16.17."
 
-while [ $ip -le 254 ]
+ip=1
+ipfinal = 254
+
+while [ $ip -le $ipfinal ]
 do
-	sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url | sh"&
+	sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
 	ip=$((ip+1))
 done
 
