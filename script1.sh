77efa34dac3f7ce72a5fae8e863c8a4efdfad1a9
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-23 22:50:16 -0300

    Alt

--- script1.sh
+++ script1.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+# Pedro Almeida - pedroalmeida173@gmail.com
+
+opcao='enabled' 
+
 echo "*** Iniciando ***"
 rm /tmp/tempconfig
 
@@ -5,8 +10,8 @@ echo "*** Apagando linha de configuracoes do discovery ***"
 grep -v "discovery" /tmp/system.cfg > /tmp/tempconfig
 
 echo "*** Inserindo nova configuracao ***"
-echo "discovery.cdp.status=disabled" >> /tmp/tempconfig
-echo "discovery.status=disabled" >> /tmp/tempconfig
+echo "discovery.cdp.status=$opcao" >> /tmp/tempconfig
+echo "discovery.status=$opcao" >> /tmp/tempconfig
 
 echo "*** Sanvando na Flash ***"
 cfgmtd -w -p /etc/
