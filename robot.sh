8a77a90e62cc089855846ad436c4bb22d60fc95d
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-24 06:01:29 -0300

    Inserindo usuarios

--- robot.sh
+++ robot.sh
@@ -2,17 +2,66 @@
 # Pedro Almeida - pedroalmeida173@gmail.com
 
 port=22
-
-user=cliente
-pass=radio@cliente
 network="172.16.17."
+ipfinal=254
 
+
+#-----------------------------------------------------
+user=marciel
+pass=md0986
 ip=1
-ipfinal = 254
 
 while [ $ip -le $ipfinal ]
 do
-	sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
-	ip=$((ip+1))
+        sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
+        ip=$((ip+1))
 done
 
+sleep 120
+echo '' > /home/veloce/.ssh/known_hosts
+#-----------------------------------------------------
+
+#-----------------------------------------------------
+user=nsp
+pass=netmpp
+ip=1
+
+while [ $ip -le $ipfinal ]
+do
+        sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
+        ip=$((ip+1))
+done
+
+sleep 120
+echo '' > /home/veloce/.ssh/known_hosts
+#-----------------------------------------------------
+
+#-----------------------------------------------------
+user=vlc
+pass=vlcpp10
+ip=1
+
+while [ $ip -le $ipfinal ]
+do
+        sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
+        ip=$((ip+1))
+done
+
+sleep 120
+echo '' > /home/veloce/.ssh/known_hosts
+#-----------------------------------------------------
+
+#-----------------------------------------------------
+user=internet
+pass=radio@cliente
+ip=1
+
+while [ $ip -le $ipfinal ]
+do
+        sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/PedroDeAlmeida/ubnt_robot/master/script1.sh | sh"&
+        ip=$((ip+1))
+done
+
+sleep 120
+echo '' > /home/veloce/.ssh/known_hosts
+#-----------------------------------------------------
